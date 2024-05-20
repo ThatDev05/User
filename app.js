935b2760253f8cda694dd0bac71c655b69b7bebf
thatdev05 <deepb9945@gmail.com>
2024-05-20 18:37:24 +0530

    user first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const usermodel = require('./models/user')
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.render("index")
+  })
+
+  app.get('/read',  async function (req, res) {
+    let user = await usermodel.find();
+    res.render("read", {user})
+    })
+
+  app.post('/create', async function (req, res) {
+      let {name ,email, img } =req.body;
+
+     let createduser = await usermodel.create({
+        name,
+        email,
+        img
+      })
+
+      res.redirect(`/read`)
+    })
+
+  app.get('/edit', function (req, res) {
+    res.render("edit")
+    })
+  
+
+
+app.listen(3000)
+
+
+
+
